c882db15fa2721f14e8a8e384d8f1b6f4bceb90f
kyeung2 <yeung.kye@pm.me>
2024-09-19 15:47:22 +0100

    initial Spring Initialised project

---
+++ src/test/java/nimbus/onepiece/devilfruits/ApplicationTests.java
@@ -0,0 +1,13 @@
+package nimbus.onepiece.devilfruits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
