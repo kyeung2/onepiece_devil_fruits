a7bc96cf1af734f21f6568e3a519b87e36e3c729
kyeung2 <yeung.kye@pm.me>
2024-09-26 21:52:43 +0100

    wiremock in SDK tests

--- sdk/src/test/java/com/nimbus/onepiece/devilfruits/sdk/client/DefaultBlockingDevilFruitsClientTest.java
+++ sdk/src/test/java/com/nimbus/onepiece/devilfruits/sdk/client/DefaultBlockingDevilFruitsClientTest.java
@@ -1,32 +1,52 @@
 package com.nimbus.onepiece.devilfruits.sdk.client;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nimbus.onepiece.devilfruits.sdk.dto.DevilFruitDto;
 import com.nimbus.onepiece.devilfruits.sdk.dto.StrawHatDevilFruits;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 @ActiveProfiles("integration")
+@AutoConfigureWireMock(port = 8081)
 class DefaultBlockingDevilFruitsClientTest {
 
     @Autowired
     BlockingDevilFruitsClient objectUnderTest;
+    @Autowired
+    ObjectMapper objectMapper;
 
     @Test
     void getDevilFruit() {
-        //TODO set up a wiremock server to simulate the devil fruit service running.
         //given
-        String code = StrawHatDevilFruits.GOMU_GOMU_NO_MI.code();
+        DevilFruitDto fruit = StrawHatDevilFruits.GOMU_GOMU_NO_MI;
+        stubGetDevilFruitResponse(fruit);
         //when
-        Optional<DevilFruitDto> actual = objectUnderTest.getDevilFruit(code);
+        Optional<DevilFruitDto> actual = objectUnderTest.getDevilFruit(fruit.code());
         //then
         assertTrue(actual.isPresent());
+        assertEquals(fruit, actual.get());
     }
 
+    //TODO add test for exception
+    //TODO add test for getAll
+
+    @SneakyThrows
+    private void stubGetDevilFruitResponse(DevilFruitDto fruit) {
+        stubFor(get(urlEqualTo("/devilfruits/%s".formatted(fruit.code())))
+                .willReturn(aResponse()
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(objectMapper.writeValueAsString(fruit))
+                        .withStatus(200)));
+    }
 }
