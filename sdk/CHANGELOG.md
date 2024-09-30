178531929a55bd75bc55ebbbdacf570872ea94f9
kyeung2 <yeung.kye@pm.me>
2024-09-30 12:57:20 +0100

    publishing settings for SDK

--- sdk/CHANGELOG.md
+++ sdk/CHANGELOG.md
@@ -1,3 +1,9 @@
 - V1.0.0 26/09/2024
   - `BlockingDevilFruitsClient` added.
   - auto configuration of client controlled via config properties added.
+
+- V1.1.0 30/09/2024
+  - expose `com.nimbus.onepiece.devilfruits.interfaces` as part of the SDK public API.
+
+- V1.1.1 30/09/2024
+  - fix bug where SDK did not have the META-INF configuration for auto config in consumers 
--- sdk/src/main/java/com/nimbus/onepiece/devilfruits/sdk/config/DevilFruitsClientAutoConfiguration.java
+++ sdk/src/main/java/com/nimbus/onepiece/devilfruits/sdk/config/DevilFruitsClientAutoConfiguration.java
@@ -3,15 +3,15 @@ package com.nimbus.onepiece.devilfruits.sdk.config;
 import com.nimbus.onepiece.devilfruits.sdk.client.DefaultDevilFruitsClient;
 import com.nimbus.onepiece.devilfruits.sdk.client.DevilFruitsClient;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 
-@Configuration
+@AutoConfiguration
 @ConditionalOnProperty(name = "onepiece.devilfruits.sdk.client.enabled", havingValue = "true", matchIfMissing = false)
 @EnableConfigurationProperties(DevilFruitsClientProperties.class)
 public class DevilFruitsClientAutoConfiguration {
@@ -28,6 +28,5 @@ public class DevilFruitsClientAutoConfiguration {
     public DevilFruitsClient devilFruitsClient(@Qualifier("devilFruitsWebClient") WebClient webClient) {
         return new DefaultDevilFruitsClient(webClient);
     }
-
 
 }
